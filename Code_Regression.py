2dd1284c4cf0fbcba9847522f10595928cab5dc0
Roman <61010440+Aettio@users.noreply.github.com>
2021-03-14 15:11:13 +0200

    Update Code_Regression.py

--- Code_Regression.py
+++ Code_Regression.py
@@ -4,17 +4,26 @@ from sklearn.model_selection import cross_val_score, train_test_split, GridSearc
 
 df = pd.read_csv("data.csv")
 
+# Убираем целевой столбец:
+
 X = df[['acousticness', 'danceability', 'duration_ms', 'energy', 'explicit', 
         'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'popularity', 'speechiness',
         'tempo', 'valence']]
 y = df['year']
 
+# Разбиваем выборку на тренировочную и тестовую:
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# 
+# Создание:
 
 model = XGBRegressor()
+
+# Тренировка на выбраных данных:
+
 model.fit(X_train, y_train)
+
+# Смотрим оценку и CSV:
 
 print('Model Check')
 print('Model score:', model.score(X_test, y_test))
